f314faabbfb377fc39b62a7c57673c56d99d0e06
FranckRomer <pacometal95@gmail.com>
2023-02-24 06:49:25 -0600

    cambios a logos

--- src/modals/ModalProject.tsx
+++ src/modals/ModalProject.tsx
@@ -22,13 +22,14 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
         {
             name: "Settings",
             text: "Esta es una pagina de configuraciones con el proposito de modificar parametros en los dispositivos electronicos. La pagina cuenta con un inicio de usuarios administrativos e instaladores para recopilar que usuario que ah realizado un cambio o ajuste",
-            img: "/projects/configuraciones.png",
+            img: "/projects/config2.png",
             skills: [
                 { name: "React", img: "/SKILLS/react.png" },
                 { name: "Next js", img: "/SKILLS/nextjs.png" },
                 { name: "Arduino", img: "/SKILLS/arduino.png" },
                 { name: "MongoDB", img: "/SKILLS/mongodb.png" },
                 { name: "Css", img: "/SKILLS/css.png" },
+                
             ],
             links: [
                 { name: "Git", img: "/social/github.png", url: "/" },
@@ -37,7 +38,7 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
         {
             name: "Buses",
             text: "Este es un proyecto de visualizacion de los estados de los autobuses. En un principio comenzo como una pagina de visualizacion para los instaladores, posterior mente fue usada para administrativos y operadores, por lo que se agrego un apartado para configurar datos.",
-            img: "/projects/camiones.png",
+            img: "/projects/train2.png",
             skills: [
                 { name: "React", img: "/SKILLS/react.png" },
                 { name: "Nest", img: "/SKILLS/nest.png" },
@@ -52,13 +53,14 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
         {
             name: "Ads",
             text: 'Proyecto de anuncios para camiones, con el nombre de "proxima parada", muestra la ruta del autobus, en que parada se encuentra actualmente y cada cierto tiempo configurable se muestran anuncios patrocinados.',
-            img: "/projects/anuncios.png",
+            img: "/projects/ads2.png",
             skills: [
                 { name: "React", img: "/SKILLS/react.png" },
                 { name: "Next js", img: "/SKILLS/nextjs.png" },
                 { name: "Css", img: "/SKILLS/css.png" },
                 { name: "Raspberry", img: "/SKILLS/raspberry.png" },
                 { name: "Python", img: "/SKILLS/python.png" },
+                { name: "Figma", img: "/SKILLS/figma.png" },
             ],
             links: [
                 { name: "Git", img: "/social/github.png", url: "/" },
@@ -67,10 +69,15 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
         {
             name: "Domotica",
             text: "Proyecto de domotica, con el proposito de automatizar hogares se creo tanto los dispositivos, arquitectura, paginas de usario y paginas de administrativos para lograr el cometido.",
-            img: "/projects/domotica.png",
+            img: "/projects/dana2.png",
             skills: [
                 { name: "React", img: "/SKILLS/react.png" },
-                { name: "Nest", img: "/SKILLS/nest.png" },
+                { name: "Next js", img: "/SKILLS/nextjs.png" },
+                { name: "Css", img: "/SKILLS/css.png" },
+                { name: "MongoDB", img: "/SKILLS/mongodb.png" },
+                { name: "Arduino", img: "/SKILLS/arduino.png" },
+                { name: "C++", img: "/SKILLS/c++.png" },
+                { name: "Microchip", img: "/SKILLS/microchip.png" },
             ],
             links: [
                 { name: "Git", img: "/social/github.png", url: "/" },
@@ -114,7 +121,7 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
                                 {project.text}
                             </p>
                             <Image
-                                className='peer w-4/5 m-auto hover:scale-150'
+                                className='peer w-4/5 m-auto hover:scale-150 transition-all duration-500'
                                 src={project.img}
                                 alt={project.name}
                                 width={500}
@@ -122,7 +129,7 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
                             />
                         </div>
 
-                        <div className="grid sm:grid-cols-2 gap-14 gap-y-4">
+                        <div className="grid gap-14 gap-y-4">
                             <div className="">
                                 <h2 className="text-3xl my-4">
                                     Herramientas
@@ -131,7 +138,7 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
                                     {project.skills.map((skill, index) => (
                                         <div key={index} className='grid '>
                                             <Image
-                                                className='peer w-10 sm:w-12 animate-spin-slow hover:animate-none hover:scale-125 m-auto'
+                                                className='peer w-10 sm:w-12 animate-spin-slow hover:animate-none hover:scale-125 m-auto '
                                                 src={skill.img}
                                                 alt={skill.name}
                                                 width={77}
@@ -142,9 +149,9 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
                                     ))}
                                 </div>
                             </div>
-                            <div>
-                                <h2 className="text-3xl my-4">Links</h2>
-                                <div className='grid grid-auto gap-y-4 md:gap-y-12 gap-6 '>
+                            {/* <div>
+                                <h2 className="text-3xl my-4 text-center">Links</h2>
+                                <div className='grid grid-cols-2 gap-y-4 md:gap-y-12 gap-6 '>
                                     {project.links.map((link, index) => (
                                         <Link href={link.url} key={index} className='grid '>
                                             <Image
@@ -158,7 +165,7 @@ const ModalProject = ({ Modal, setModal, SelecttProyect }: any) => {
                                         </Link>
                                     ))}
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
 
                     </div>
